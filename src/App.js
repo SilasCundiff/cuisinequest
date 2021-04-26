0cc57730ee06b424e6bfe9d0f72824bef3de4216
SilasCundiff <silinski23@gmail.com>
2021-04-26 15:03:04 -0400

    add basic project structure, add hero background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+// App imports
+// none for now
+// Header imports
+
 function App() {
   return (
-    <div className='bg-gray-700'>
-      <h1 className='text-6xl text-green-500'>CuisineQuest</h1>
-      <p className='text-4xl text-green-50'>Every Meal is a Journey</p>
+    <div className='bg-gray-900'>
+      <Nav />
+      <Header />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+export const Header = () => {
+  return (
+    <div className='flex flex-col bg-hero-img bg-grey-50 bg-right-bottom bg-cover bg-no-repeat h-screen'>
+      Hero
+    </div>
+  );
+};
+
+export const Nav = () => {
+  return (
+    <div className='text-4xl text-green-400 bg-transparent fixed'>
+      CuisineQuest
+    </div>
+  );
+};
+
+export const Footer = () => {
+  return <div className='text-6xl fixed'>test</div>;
+};
